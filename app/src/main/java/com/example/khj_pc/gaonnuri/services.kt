c19677e5fcb4880fa44a67a56d50283ff069f553
baehyeonbin <baehynebin@hotmail.co.kr>
2018-08-19 23:01:58 +0900

    로그인 기능 구현 완료

--- app/src/main/java/com/example/khj_pc/gaonnuri/services.kt
+++ app/src/main/java/com/example/khj_pc/gaonnuri/services.kt
@@ -6,27 +6,27 @@ import retrofit2.http.*
 
 interface UserService {
 
-    @POST("/users/authenticate")
-    fun login(@Body user : User) : Call<LoginResult>
+    @POST("user/authenticate")
+    fun login(@Body user : LoginUser) : Call<LoginResult>
 
-    @POST("/users/register")
+    @POST("user/register")
     fun register(@Body user : User) : Call<Result>
 
     //토큰 필요
-    @POST("/users/profile/{id}")
+    @POST("user/profile/{id}")
     fun getUserInfo(@Path("id") id : String) : Call<UserResult>
 }
 
 interface RoomService {
-    @POST("/room/create")
+    @POST("room/create")
     fun createRoom(@Body room : Room) : Call<Result>
 
-    @POST("/room/search/{sub_string}")
+    @POST("room/search/{sub_string}")
     fun search(@Path("sub_string") subString : String) : Call<RoomResult>
 
-    @POST("/room/enter/{id}/{objId}")
+    @POST("room/enter/{id}/{objId}")
     fun enter(@Path("id") id : String, @Path("objId") objectId : String) : Call<Result>
 
-    @GET("/room/get_room/{objId}")
+    @GET("room/get_room/{objId}")
     fun getSingleRoomInfo(@Path("obj_id") objectId : String) : Call<SingleRoomResult>
 }
