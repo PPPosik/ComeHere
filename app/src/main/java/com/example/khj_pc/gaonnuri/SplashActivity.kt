d00a7a801ffbc0f36695d907f0ed1a8f0e2e4cb4
정승욱 <wjdtmddnr24@naver.com>
2018-08-29 05:02:30 +0900

    스플래시다

--- app/src/main/java/com/example/khj_pc/gaonnuri/SplashActivity.kt
+++ app/src/main/java/com/example/khj_pc/gaonnuri/SplashActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
 
         Thread(Runnable {
             Thread.sleep(1000)
