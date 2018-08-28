9a1231bafb24a3f62f3b6ab18e6753f2d4d90323
정승욱 <wjdtmddnr24@naver.com>
2018-08-29 02:43:33 +0900

    설문 통계하는중

---
+++ app/src/main/java/com/example/khj_pc/gaonnuri/SurveyChartActivity.java
@@ -0,0 +1,14 @@
+package com.example.khj_pc.gaonnuri;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SurveyChartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey_chart);
+    }
+
+}
