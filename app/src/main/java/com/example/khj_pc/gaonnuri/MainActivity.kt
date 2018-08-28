56baa7b91e09cdd87e5cb7de58419455d44e49ff
정승욱 <wjdtmddnr24@naver.com>
2018-08-29 01:32:59 +0900

    설문 하기 페이지 완료 힘드러러어어

--- app/src/main/java/com/example/khj_pc/gaonnuri/MainActivity.kt
+++ app/src/main/java/com/example/khj_pc/gaonnuri/MainActivity.kt
@@ -34,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        var intent: Intent = Intent(this, SurveyCreateActivity::class.java)
+        var intent: Intent = Intent(this, SurveyDoActivity::class.java)
         startActivity(intent)
 
         val toggle = ActionBarDrawerToggle(
