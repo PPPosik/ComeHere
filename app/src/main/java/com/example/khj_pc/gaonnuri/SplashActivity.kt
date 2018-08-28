51510e464b130d48d30434bced4e1e01434ed161
정승욱 <wjdtmddnr24@naver.com>
2018-08-29 04:54:08 +0900

    스플래시

--- app/src/main/java/com/example/khj_pc/gaonnuri/SplashActivity.kt
+++ app/src/main/java/com/example/khj_pc/gaonnuri/SplashActivity.kt
@@ -4,13 +4,16 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 
-class SplashActivity : Activity(){
+class SplashActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Thread.sleep(1000)
-        var intent : Intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
+        Thread(Runnable {
+            Thread.sleep(1000)
+            var intent: Intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }).start()
+
     }
 }
